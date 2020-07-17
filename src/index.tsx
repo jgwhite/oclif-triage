537cc122dc3bcf9b0e8da3c4aa9c5691be0e5b8c
Jamie White <jameswhite@salesforce.com>
2020-07-17 14:31:56 +0200

    Setup ink and react and friends

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,8 @@
 import {Command, flags} from '@oclif/command'
 import {Octokit} from '@octokit/rest'
 import React from 'react'
-import {render, Text} from 'ink'
+import {render, Box, Text} from 'ink'
+import Table from 'ink-table'
 
 class OclifTriage extends Command {
   static description = 'Jamie’s OCLIF triage tool'
@@ -14,15 +15,43 @@ class OclifTriage extends Command {
   static args = [{name: 'file'}]
 
   async run() {
-    const octokit = new Octokit()
-    const {data} = await octokit.repos.listForOrg({
+    const auth = process.env.OCLIF_TRIAGE_TOKEN
+    if (!auth) {
+      this.error('Please set OCLIF_TRIAGE_TOKEN in your environment')
+    }
+    const octokit = new Octokit({auth})
+    let {data: repos} = await octokit.repos.listForOrg({
       org: 'oclif',
-      type: 'public',
+      public: true,
     })
+    let rows = await Promise.all(
+      repos.map(async (repo) => {
+        const {data: issues} = await octokit.issues.listForRepo({
+          repo: repo.name,
+          owner: repo.owner.login,
+          state: 'open'
+        })
+        const {data:pulls} = await octokit.pulls.list({
+          repo: repo.name,
+          owner: repo.owner.login,
+          state: 'open'
+        })
 
-    render(
-      <Text bold color="green">Repos: {data.length}</Text>
+        return {
+          repo: repo.full_name,
+          issues: issues.length,
+          pulls: pulls.length
+        }
+      })
     )
+
+    rows = rows.filter(row => row.issues || row.pulls)
+
+    rows.sort((a, b) => (b.issues + b.pulls) - (a.issues + a.pulls))
+
+    rows = rows.slice(0, 10)
+
+    render(<Table data={rows} />)
   }
 }
 
